fd570556d5095064a1c62c4898292b28d101e508
Justinjolly <83817846+Justinjolly@users.noreply.github.com>
2021-12-13 22:59:13 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*************************************
+ * File    : HelloWorld.java
+ * Program : To print Hello World
+ * Author  : Justin Jolly
+ * Version : 1.0
+ **************************************/
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("Hello World");
+	}
+
+}
