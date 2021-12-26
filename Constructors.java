e99aeb446bb015a5b72e3a97fa4f7ba703a62361
Justinjolly <83817846+Justinjolly@users.noreply.github.com>
2021-12-26 10:02:35 +0530

    Add files via upload

---
+++ Constructors.java
@@ -0,0 +1,19 @@
+/*******************************************
+ * File    : Constructors.java
+ * Program : To demonstrate constructor
+ * Author  : Justin Jolly
+ * Version : 1.0
+ * Date    : 09-12-2021
+ *******************************************/
+class Bike{
+	Bike(){// constructor
+		System.out.println("Bike is created");
+	}
+}
+public class Constructors {
+	public static void main(String[] args) {
+		Bike r = new Bike();
+
+	}
+
+}
